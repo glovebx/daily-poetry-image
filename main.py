1eaebc5dffaf9b00f7e85eb28ed68305dc56bea3
glovebx <glovebx@glovebxdeMacBook-Pro-2.local>
2024-04-20 01:24:01 +0800

    bugfix

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def upload_s3(key, img_content, s3_config):
                             region_name='auto')
     s3 = session.client('s3', endpoint_url=s3_config.get('endpoint_url'))    
     obj = s3.put_object(Body=img_content, Key=key, Bucket=s3_config.get('bucket_name'), ContentType="image/png", ACL="public-read")
-    response = {attr: getattr(obj, attr) for attr in ['e_tag', 'version_id']}
+    # response = {attr: getattr(obj, attr) for attr in ['e_tag', 'version_id']}
+    response = {attr: getattr(obj, attr) for attr in ['version_id', ]}
     return f'{s3_config.get("img_access_url")}/{key}?versionId={response["version_id"]}'
 
 
